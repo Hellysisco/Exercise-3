5ba2d5385c70a8aade7ed43b13b0b298c0b7b3a2
MUSIMBI FRANCIS <128372690+Hellysisco@users.noreply.github.com>
2024-09-30 10:28:51 +0300

    Loops
    Loops

---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub btnDisplayNumbers_Click(sender As Object, e As EventArgs) Handles btnDisplayNumbers.Click
+        ' Clear any previous items from the ListBox
+        lstNumbers.Items.Clear()
+
+        ' For loop to display numbers from 1 to 10
+        For i As Integer = 1 To 10
+            lstNumbers.Items.Add(i)
+        Next
+    End Sub
+
+    Private Sub btnCountdown_Click(sender As Object, e As EventArgs) Handles btnCountdown.Click
+        ' Get the user input number
+        Dim num As Integer
+        If Integer.TryParse(txtNumber.Text, num) Then
+            ' Clear previous items in the ListBox
+            lstCountdown.Items.Clear()
+
+            ' While loop to count down from the entered number to zero
+            While num >= 0
+                lstCountdown.Items.Add(num)
+                num -= 1
+            End While
+        Else
+            MessageBox.Show("Please enter a valid number.")
+        End If
+    End Sub
+End Class
